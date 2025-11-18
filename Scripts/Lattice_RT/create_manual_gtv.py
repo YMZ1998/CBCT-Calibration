31405dcc39e85c0e638b8d0e0c15596503849050
LiJin <1320439118@qq.com>
2025-11-18 17:46:23 +0800

    Update Lattice RT.

--- Scripts/Lattice_RT/create_manual_gtv.py
+++ Scripts/Lattice_RT/create_manual_gtv.py
@@ -10,25 +10,23 @@ def array_to_sitk(arr, spacing=(1.0, 1.0, 1.0), origin=(0, 0, 0)):
     return img
 
 
-def create_manual_gtv(shape=(64, 64, 64), spacing=(1.0, 1.0, 1.0)):
-    """
-    shape: (z,y,x)
-    spacing: voxel spacing (sx, sy, sz)
-    返回: SimpleITK 图像，二值 mask
-    """
+def create_manual_gtv(shape=(64, 64, 64), spacing=(2.0, 2.0, 2.0), type='sphere'):
     sz, sy, sx = shape
-    gtv_arr = np.zeros(shape, dtype=np.uint8)
+    arr = np.zeros(shape, dtype=np.uint8)
 
-    # 示例 1：在中心创建一个球形 GTV
-    center = np.array([sz // 2, sy // 2, sx // 2])
-    radius_vox = min(sz, sy, sx) // 4  # 半径 = 体积最小边/4
+    if type == 'sphere':
+        center = np.array([sz // 2, sy // 2, sx // 2])
+        radius = min(sz, sy, sx) // 4
+        zz, yy, xx = np.meshgrid(np.arange(sz), np.arange(sy), np.arange(sx), indexing='ij')
+        dist2 = (zz - center[0]) ** 2 + (yy - center[1]) ** 2 + (xx - center[2]) ** 2
+        arr[dist2 <= radius ** 2] = 1
+    elif type == 'cube':
+        arr[sz // 4:3 * sz // 4, sy // 4:3 * sy // 4, sx // 4:3 * sx // 4] = 1
 
-    zz, yy, xx = np.meshgrid(np.arange(sz), np.arange(sy), np.arange(sx), indexing="ij")
-    dist2 = (zz - center[0]) ** 2 + (yy - center[1]) ** 2 + (xx - center[2]) ** 2
-    gtv_arr[dist2 <= radius_vox ** 2] = 1
+    img = sitk.GetImageFromArray(arr)
+    img.SetSpacing(spacing)
+    return img
 
-    gtv_img = array_to_sitk(gtv_arr, spacing=spacing)
-    return gtv_img
 
 
 if __name__ == "__main__":
